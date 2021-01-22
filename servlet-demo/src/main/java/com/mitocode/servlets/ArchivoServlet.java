82ff8275c4c58c3f4d5e03fcef90c840a3056ac8
Pablo Rodriguez <prodriguez769@gmail.com>
2021-01-22 17:34:26 -0500

    Ejemplo de servlet para obtner un pdf (lee un pdf y lo envia como respuesta del servlet)

--- servlet-demo/src/main/java/com/mitocode/servlets/ArchivoServlet.java
+++ servlet-demo/src/main/java/com/mitocode/servlets/ArchivoServlet.java
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/archivo")
 public class ArchivoServlet extends HttpServlet {
 
+	/*
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("image/jpg");
@@ -26,6 +27,38 @@ public class ArchivoServlet extends HttpServlet {
 		
 		//Es necesario crear la carpeta /resources/image y colocar el archivo. Esto en src/main/webapp
 		String ruta = "/resources/image/imagen.jpg";//esta imagen se coloca a la altura de la carpeta webapp, dado que es lo que está visible al proyecto
+		InputStream in = getServletContext().getResourceAsStream(ruta);
+		
+		//Los buffered son clases que permiten leer y escribir en bloque, para no estar haciendolo byte a byte
+		BufferedInputStream bin = new BufferedInputStream(in);//Canal que va leyendo la imagen
+		BufferedOutputStream bout = new BufferedOutputStream(out);//canal que va escribiendo en la imagen
+		
+		int ch = 0;
+		
+		while((ch = bin.read()) != -1) {//lee en bloques hasta que no haya más que leer
+			bout.write(ch);
+		}
+		
+		//cerramos para liberar recursos y que no se sature el ancho de banda
+		bin.close();
+		in.close();
+		bout.close();
+		out.close();
+	}
+	*/
+	
+	//descargar PDF
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("application/pdf");
+		
+		//Con la siguiente linea se especifica que la salida es un adjunto por tanto se descargará el pdf, sino se especifica se visualziará el PDF en el browser
+		resp.setHeader("Content-Disposition", "attachment; filename=\"nombrePdf.pdf\"");		
+		
+		ServletOutputStream out = resp.getOutputStream();
+		
+		//Es necesario crear la carpeta /resources/image y colocar el archivo. Esto en src/main/webapp
+		String ruta = "/resources/pdf/servlets.pdf";//esta imagen se coloca a la altura de la carpeta webapp, dado que es lo que está visible al proyecto
 		InputStream in = getServletContext().getResourceAsStream(ruta);
 		
 		//Los buffered son clases que permiten leer y escribir en bloque, para no estar haciendolo byte a byte
