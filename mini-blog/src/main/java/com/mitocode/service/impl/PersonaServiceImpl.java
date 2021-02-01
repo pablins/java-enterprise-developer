0b0dc2a948f1e1f16342adcdadd548479058f566
Pablo Rodriguez <prodriguez769@gmail.com>
2021-02-01 11:28:53 -0500

    Se crea objeto de comunicación entre capa service y DAO. Por ahora sólo se usa como puente/pasarela, pero posteriormente la capa service tendrá lógica de negocio y la capa DAO recibirá la información procesada lista para la comunicación con la DB

--- mini-blog/src/main/java/com/mitocode/service/impl/PersonaServiceImpl.java
+++ mini-blog/src/main/java/com/mitocode/service/impl/PersonaServiceImpl.java
@@ -2,34 +2,37 @@ package com.mitocode.service.impl;
 
 import java.util.List;
 
+import com.mitocode.dao.IPersonaDAO;
+import com.mitocode.dao.impl.PersonaDAOImpl;
 import com.mitocode.model.Persona;
 import com.mitocode.service.IPersonaService;
 
 public class PersonaServiceImpl implements IPersonaService {
+	
+	private IPersonaDAO dao;
+	
+	public PersonaServiceImpl() {
+		dao = new PersonaDAOImpl();//Forma tradicional de creación de instancia para comunicar el service con el DAO
+	}
 
 	public Integer registrar(Persona per) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.registrar(per);
 	}
 
 	public Integer modificar(Persona per) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.modificar(per);
 	}
 
 	public Integer eliminar(Persona per) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.eliminar(per);
 	}
 
 	public List<Persona> listar() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.listar();
 	}
 
 	public Persona listarPorId(Persona per) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.listarPorId(per);
 	}
 
 }
