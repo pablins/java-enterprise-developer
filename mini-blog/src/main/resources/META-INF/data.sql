ad1f645a0aeabca084723598453bd9111621e48b
Pablo Rodriguez <prodriguez769@gmail.com>
2021-02-05 17:57:01 -0500

    Se crea archivo SQL data.sql para que se ejecute cuando se despliegue la app. En el persistence.xml se define su ejecución

---
+++ mini-blog/src/main/resources/META-INF/data.sql
@@ -0,0 +1,7 @@
+--********************************************
+--Script para ser ejecutado por el persistence.xml cuando se levante la aplicación
+--********************************************
+
+--INSERTAMOS ROLES
+INSERT INTO Rol (tipo) VALUES ('ADMIN');
+INSERT INTO Rol (tipo) VALUES ('USER');
