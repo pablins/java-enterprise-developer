b7f29661463ef757adc830ff05bce81f3ce74a4b
Pablo Rodriguez <prodriguez769@gmail.com>
2021-02-05 17:39:07 -0500

    Creamos en el IRolDAO método de asignar para poder asignar roles cuando se creen los usuarios

--- mini-blog/src/main/java/com/mitocode/dao/IRolDAO.java
+++ mini-blog/src/main/java/com/mitocode/dao/IRolDAO.java
@@ -1,10 +1,17 @@
 package com.mitocode.dao;
 
+import java.util.List;
+
 import javax.ejb.Local;
 
 import com.mitocode.model.Rol;
+import com.mitocode.model.Usuario;
+import com.mitocode.model.UsuarioRol;
 
 @Local
 public interface IRolDAO extends ICRUD<Rol>{
+	
+	//Método para asignar roles a los usuarios cuando se cree el usuario
+	Integer asignar(Usuario usuario, List<UsuarioRol> roles);
 
 }
--- mini-blog/src/main/java/com/mitocode/dao/impl/RolDAOImpl.java
+++ mini-blog/src/main/java/com/mitocode/dao/impl/RolDAOImpl.java
@@ -10,6 +10,8 @@ import javax.persistence.Query;
 
 import com.mitocode.dao.IRolDAO;
 import com.mitocode.model.Rol;
+import com.mitocode.model.Usuario;
+import com.mitocode.model.UsuarioRol;
 
 @Stateless
 public class RolDAOImpl implements IRolDAO, Serializable {
@@ -52,6 +54,37 @@ public class RolDAOImpl implements IRolDAO, Serializable {
 		List<Rol> lista = (List<Rol>)query.getResultList();//Se recomienda el casteo explicito, dado que por default devuelve un object
 		//Código limpio recomienda que las capas no devuelvan NULL, por tanto en caso de no encontrar el objeto devolveremos una instancia de Rol vacia
 		return lista != null && !lista.isEmpty() ? lista.get(0) : new Rol();
+	}
+
+	@Override
+	public Integer asignar(Usuario usuario, List<UsuarioRol> roles) {
+		
+		//1-Primero eliminamos todos los roles que ya tenga el usuario
+		Query query = em.createNativeQuery("DELETE FROM usuario_rol ur WHERE ur.id_usuario = ?1");//en esta ocasión estamos usando un query nativo, SQL normal
+		query.setParameter(1, usuario.getPersona().getId());
+		query.executeUpdate();//Realizamos la ejecución del query
+		
+		
+		//2-Insertamos los nuevos roles
+//		int i = 0;
+		//Truco para poder usar variable local
+		int[] i = { 0 };//Iniciamos el arreglo asignando el valor cero a la primera posicion
+		//iteramos la lista de roles para insertar los nuevos roles
+		roles.forEach(r -> {
+			em.persist(r);//IMPORTANTE: El EntityManager se satura cuando se hacen muchas persistencias dentro de un bucle
+			
+			//Tip - Limpiamos el EntityManager cada N iteraciones
+			//if(i % 100 == 0) {//cada 100 iteraciones hacemos una limpieza
+			if(i[0] % 100 == 0) {//Verificamos el valor en la posicion 0
+				em.flush();
+				em.clear();
+			}
+			
+//			i++;//Las lambdas son funciones anónimas y no tienen visibilidad a lo que hay hacia afuera de ella, salvo que sea atributo de clase
+			i[0]++; 
+		});
+		
+		return i[0];//Retornamos el valor del contador (lo que se nos ocurra)
 	}
 
 }
