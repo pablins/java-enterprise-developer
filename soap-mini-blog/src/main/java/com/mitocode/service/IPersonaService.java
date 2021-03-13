0a149cf070b2190df71fbd816f5f7953fb40d187
Pablo Rodriguez <prodriguez769@gmail.com>
2021-03-12 20:12:45 -0500

    Agregamos una operación que devuelve una lista de String

--- soap-mini-blog/src/main/java/com/mitocode/service/IPersonaService.java
+++ soap-mini-blog/src/main/java/com/mitocode/service/IPersonaService.java
@@ -1,9 +1,13 @@
 package com.mitocode.service;
 
+import java.util.List;
+
 public interface IPersonaService {
 	
 	String saludar();
 	
 	String saludar(String nombre);
+	
+	List<String> listar();
 
 }
--- soap-mini-blog/src/main/java/com/mitocode/service/impl/PersonaServiceImpl.java
+++ soap-mini-blog/src/main/java/com/mitocode/service/impl/PersonaServiceImpl.java
@@ -1,6 +1,9 @@
 package com.mitocode.service.impl;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
@@ -20,6 +23,38 @@ public class PersonaServiceImpl implements IPersonaService, Serializable {
 	@Override
 	public String saludar(String nombre) {
 		return "Hola " + nombre;
+	}
+
+	@WebMethod
+	@Override
+	public List<String> listar() {
+		String[] arregloPaises = { "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua & Deps", "Argentina",
+				"Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados",
+				"Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia Herzegovina", "Botswana",
+				"Brazil", "Brunei", "Bulgaria", "Burkina", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde",
+				"Central African Rep", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo",
+				"Congo {Democratic Rep}", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark",
+				"Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador",
+				"Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia",
+				"Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana",
+				"Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland {Republic}",
+				"Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati",
+				"Korea North", "Korea South", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho",
+				"Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi",
+				"Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "México",
+				"Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar {Burma}",
+				"Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway",
+				"Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Perú", "Philippines", "Poland",
+				"Portugal", "Qatar", "Romania", "Russian Federation", "Rwanda", "St Kitts & Nevis", "St Lucia",
+				"Saint Vincent & the Grenadines", "Samoa", "San Marino", "Sao Tome & Principe", "Saudi Arabia",
+				"Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia",
+				"Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname",
+				"Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo",
+				"Tonga", "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine",
+				"United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu",
+				"Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe" };
+		
+		return new ArrayList<>(Arrays.asList(arregloPaises));
 	}
 
 }
