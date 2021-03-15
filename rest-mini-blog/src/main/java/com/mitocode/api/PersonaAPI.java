081d57e1f034ccddf8ab60475b22843b8db74292
Pablo Rodriguez <prodriguez769@gmail.com>
2021-03-15 17:54:08 -0500

    Ejemplo devolviendo un objeto en API REST
    -Se crea una clase Persona y se devuelve un objeto de dicha clase
    -Para que devuelva un JSON en objeto Persona se especifica con @Produces el tipo de salida

--- rest-mini-blog/src/main/java/com/mitocode/api/PersonaAPI.java
+++ rest-mini-blog/src/main/java/com/mitocode/api/PersonaAPI.java
@@ -2,6 +2,9 @@ package com.mitocode.api;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.mitocode.model.Persona;
 
 //Indica que es una salida de un servicio REST
 @Path("/personas")
@@ -14,4 +17,16 @@ public class PersonaAPI {
 		return "Hola Coders!!!";
 	}
 	
+	@GET
+	@Path("/objeto")
+	//Indicamos que produzca una salida en JSON
+	//Si no se hace obtendremos: Could not find MessageBodyWriter for response object of type: com.mitocode.model.Persona of media type: text/html;charset=UTF-8
+	@Produces("application/json")//puede ser json, xml o genericos como archivos, etc
+	public Persona ejemploRetornarObjeto() {
+		Persona persona = new Persona();
+		persona.setId(1);
+		persona.setNombre("Pablins");
+		
+		return persona;
+	}
 }
